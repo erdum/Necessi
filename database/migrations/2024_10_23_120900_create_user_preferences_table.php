9b0894906c1d39cd87dbe282156f72226ec0bbfe
Max-2026 <erdum.adnan@launchbox.pk>
2024-10-23 17:17:29 +0500

    Feat(User Settings): User preferences entity implemented

---
+++ database/migrations/2024_10_23_120900_create_user_preferences_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_preferences', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->boolean('general_notifications')->default(true);
+            $table->boolean('biding_notifications')->default(true);
+            $table->boolean('transaction_notifications')->default(true);
+            $table->boolean('activity_notifications')->default(true);
+            $table->boolean('messages_notifications')->default(true);
+            $table->enum('who_can_see_connections', [
+                'public',
+                'connections',
+                'only_me'
+            ]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_preferences');
+    }
+};
