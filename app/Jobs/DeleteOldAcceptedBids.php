1143ec494cbed781b42e96a26f9483e1b6bf9677
Max-2026 <erdum.adnan@launchbox.pk>
2024-11-19 19:11:57 +0500

    Refactor(Observer): Firestore handling removed

--- app/Jobs/DeleteOldAcceptedBids.php
+++ app/Jobs/DeleteOldAcceptedBids.php
@@ -9,7 +9,6 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Carbon\Carbon;
 use App\Models\PostBid;
-use Kreait\Firebase\Factory;
 
 class DeleteOldAcceptedBids implements ShouldQueue
 {
@@ -21,14 +20,9 @@ class DeleteOldAcceptedBids implements ShouldQueue
      * @param Factory $factory
      * @return void
      */
-    public function handle(Factory $factory)
+    public function handle()
     {
         try {
-            $firebase = $factory->withServiceAccount(
-                config('firebase.projects.app.credentials')
-            );
-            $db = $firebase->createFirestore()->database();
-
             $one_day_ago = Carbon::now()->subDay();
             $bids = PostBid::where('status', 'accepted')
             ->where('updated_at', '<', $one_day_ago)
@@ -41,16 +35,6 @@ class DeleteOldAcceptedBids implements ShouldQueue
             if ($bids->isNotEmpty()) {
                 foreach ($bids as $bid) 
                 {
-                    $bid_ref = $db->collection('posts')
-                        ->document($bid->post_id)
-                        ->collection('bids')
-                        ->document($bid->user->uid);
-                    $bid_snapshot = $bid_ref->snapshot();
-
-                    if ($bid_snapshot->exists()) {
-                        $bid_ref->delete();
-                    }
-
                     $bid->delete();
                 }
             }
