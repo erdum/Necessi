9eddb06df7f05109d8cb97249e7cc6733d751fe2
Max-2026 <erdum.adnan@launchbox.pk>
2024-11-19 17:01:39 +0500

    Feat(Notification Observer): Syncing notifications with Firestore

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -4,7 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Observers\NotificationObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
+ 
+#[ObservedBy([NotificationObserver::class])]
 class Notification extends Model
 {
     use HasFactory;
---
+++ app/Observers/NotificationObserver.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Notification;
+use App\Models\ConnectionRequest;
+use Kreait\Firebase\Factory;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class NotificationObserver implements ShouldQueue
+{
+    /**
+     * Handle the Notification "created" event.
+     */
+    public function created(Notification $notification): void
+    {
+        $factory = app(Factory::class);
+        $firebase = $factory->withServiceAccount(
+            base_path()
+            .DIRECTORY_SEPARATOR
+            .config('firebase.projects.app.credentials')
+        );
+        $db = $firebase->createFirestore()->database();
+
+        $connection_request = ConnectionRequest::withTrashed()->find(
+            $notification->additional_data['connection_request_id'] ?? 0
+        );
+
+        $is_request_accepted = $connection_request?->status == 'accepted';
+        $is_request_rejected = $connection_request?->status == 'rejected';
+        $is_connection_request = (! ($is_request_accepted || $is_request_rejected)) && str_contains(
+                $notification?->body,
+                'has sent you a connection request'
+            );
+
+        $data = [
+            'title' => $notification->title,
+            'body' => $notification->body,
+            'image' => $notification->image,
+            'created_at' => $notification->created_at,
+            'is_connection_request' => $is_connection_request,
+            'is_connection_request_accepted' => $is_request_accepted,
+            'is_connection_request_rejected' => $is_request_rejected,
+            'sender_id' => $notification->additional_data['sender_id'] ?? null,
+        ];
+
+        $db->collection('notifications')->document($notification->id)
+            ->set($data);
+    }
+
+    /**
+     * Handle the Notification "updated" event.
+     */
+    public function updated(Notification $notification): void
+    {
+        $factory = app(Factory::class);
+        $firebase = $factory->withServiceAccount(
+            base_path()
+            .DIRECTORY_SEPARATOR
+            .config('firebase.projects.app.credentials')
+        );
+        $db = $firebase->createFirestore()->database();
+
+        $connection_request = ConnectionRequest::withTrashed()->find(
+            $notification->additional_data['connection_request_id'] ?? 0
+        );
+
+        $is_request_accepted = $connection_request?->status == 'accepted';
+        $is_request_rejected = $connection_request?->status == 'rejected';
+        $is_connection_request = (! ($is_request_accepted || $is_request_rejected)) && str_contains(
+                $notification?->body,
+                'has sent you a connection request'
+            );
+
+        $db->collection('notifications')->document($notification->id)->update([
+            ['path' => 'title', 'value' => $notification->title],
+            ['path' => 'body', 'value' => $notification->body],
+            ['path' => 'image', 'value' => $notification->image],
+            ['path' => 'created_at', 'value' => $notification->created_at],
+            [
+                'path' => 'is_connection_request',
+                'value' => $is_connection_request
+            ],
+            [
+                'path' => 'is_connection_request_accepted',
+                'value' => $is_request_accepted
+            ],
+            [
+                'path' => 'is_connection_request_rejected',
+                'value' => $is_request_rejected
+            ],
+            [
+                'path' => 'sender_id',
+                'value' => $notification->additional_data['sender_id'] ?? null
+            ],
+        ]);
+    }
+
+    /**
+     * Handle the Notification "deleted" event.
+     */
+    public function deleted(Notification $notification): void
+    {
+        $factory = app(Factory::class);
+        $firebase = $factory->withServiceAccount(
+            base_path()
+            .DIRECTORY_SEPARATOR
+            .config('firebase.projects.app.credentials')
+        );
+        $db = $firebase->createFirestore()->database();
+
+        $db->collection('notifications')->document($notification->id)->delete();
+    }
+
+    /**
+     * Handle the Notification "restored" event.
+     */
+    public function restored(Notification $notification): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Notification "force deleted" event.
+     */
+    public function forceDeleted(Notification $notification): void
+    {
+        //
+    }
+}
