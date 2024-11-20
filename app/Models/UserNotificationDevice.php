155bfd79ae43be3bbbb113c8013ddee5ed819b32
Max-2026 <erdum.adnan@launchbox.pk>
2024-11-20 13:55:32 +0500

    Feat: Notification device observer implemented

--- app/Models/UserNotificationDevice.php
+++ app/Models/UserNotificationDevice.php
@@ -4,7 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Observers\UserNotificationDeviceObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
+ 
+#[ObservedBy([UserNotificationDeviceObserver::class])]
 class UserNotificationDevice extends Model
 {
     use HasFactory;
---
+++ app/Observers/UserNotificationDeviceObserver.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\UserNotificationDevice;
+use Kreait\Firebase\Factory;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class UserNotificationDeviceObserver implements ShouldQueue
+{
+    /**
+     * Handle the UserNotificationDevice "created" event.
+     */
+    public function created(UserNotificationDevice $userNotificationDevice): void
+    {
+        $factory = app(Factory::class);
+        $firebase = $factory->withServiceAccount(
+            base_path()
+            .DIRECTORY_SEPARATOR
+            .config('firebase.projects.app.credentials')
+        );
+        $db = $firebase->createFirestore()->database();
+
+        $db->collection('users')->document($userNotificationDevice->user->uid)
+            ->set(
+                ['fcm' => $userNotificationDevice->fcm_token],
+                ['merge' => true]
+            );
+    }
+
+    /**
+     * Handle the UserNotificationDevice "updated" event.
+     */
+    public function updated(UserNotificationDevice $userNotificationDevice): void
+    {
+        $factory = app(Factory::class);
+        $firebase = $factory->withServiceAccount(
+            base_path()
+            .DIRECTORY_SEPARATOR
+            .config('firebase.projects.app.credentials')
+        );
+        $db = $firebase->createFirestore()->database();
+
+        $db->collection('users')->document($userNotificationDevice->user->uid)
+            ->set(
+                ['fcm' => $userNotificationDevice->fcm_token],
+                ['merge' => true]
+            );
+    }
+
+    /**
+     * Handle the UserNotificationDevice "deleted" event.
+     */
+    public function deleted(UserNotificationDevice $userNotificationDevice): void
+    {
+        $factory = app(Factory::class);
+        $firebase = $factory->withServiceAccount(
+            base_path()
+            .DIRECTORY_SEPARATOR
+            .config('firebase.projects.app.credentials')
+        );
+        $db = $firebase->createFirestore()->database();
+
+        $db->collection('users')->document($userNotificationDevice->user->uid)
+            ->set(
+                ['fcm' => null],
+                ['merge' => true]
+            );
+    }
+
+    /**
+     * Handle the UserNotificationDevice "restored" event.
+     */
+    public function restored(UserNotificationDevice $userNotificationDevice): void
+    {
+        //
+    }
+
+    /**
+     * Handle the UserNotificationDevice "force deleted" event.
+     */
+    public function forceDeleted(UserNotificationDevice $userNotificationDevice): void
+    {
+        //
+    }
+}
