1a323a7a26503b89fe79924592ec0a18f56a050e
smith-jhonson-hub <fahad.naeem@launchbox.pk>
2024-12-06 12:18:47 +0500

    Fix(Observers): Observer not firing

--- app/Models/PostComment.php
+++ app/Models/PostComment.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use App\Observers\PostCommentObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+#[ObservedBy([PostCommentObserver::class])]
 class PostComment extends Model
 {
     use HasFactory;
--- app/Models/PostLike.php
+++ app/Models/PostLike.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use App\Observers\PostLikeObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+#[ObservedBy([PostLikeObserver::class])]
 class PostLike extends Model
 {
     use HasFactory;
