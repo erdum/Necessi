0846c955f7a1a96759561338b91228fa46516a44
Max-2026 <erdum.adnan@launchbox.pk>
2024-12-24 18:21:18 +0500

    Feat: Received flags included

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -232,6 +232,8 @@ class OrderService
             'return_date' => Carbon::parse($post->end_date)->format('d M Y'),
             'chat_id' => $chat_id,
             'is_marked' => $is_started,
+            'received_by_borrower' => $order->received_by_borrower,
+            'received_by_lender' => $order->received_by_lender,
         ];
     }
 
