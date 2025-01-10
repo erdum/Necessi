6f2350b82c8b926d0babd7a0afd09af54494392a
smith-jhonson-hub <fahad.naeem@launchbox.pk>
2025-01-10 18:01:49 +0500

    Fix(Post Service): Removed accepted status post of the list

--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -1303,10 +1303,12 @@ class PostService
             return [];
         }
 
-        $received_bids = PostBid::whereIn('post_id', $user_posts)->whereNot('status', 'rejected')
-            ->with(['user:id,first_name,last_name,avatar', 'post'])
-            ->orderBy('created_at', 'desc')
-            ->get();
+        $received_bids = PostBid::whereIn('post_id', $user_posts)
+        ->whereNotIn('status', ['rejected', 'accepted'])
+        ->with(['user:id,first_name,last_name,avatar', 'post'])
+        ->orderBy('created_at', 'desc')
+        ->get();
+    
 
         return $received_bids->map(function ($received_bid) {
             return [
