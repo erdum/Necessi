fd2c101a345d6be14176a6ef717a3e69ae3ea6d7
smith-jhonson-hub <fahad.naeem@launchbox.pk>
2025-01-13 19:04:35 +0500

    Refactor(Notification observer): Fetched other_party user

--- app/Observers/NotificationObserver.php
+++ app/Observers/NotificationObserver.php
@@ -4,6 +4,7 @@ namespace App\Observers;
 
 use App\Models\ConnectionRequest;
 use App\Models\Notification;
+use App\Models\User;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Kreait\Firebase\Factory;
 
@@ -33,12 +34,9 @@ class NotificationObserver implements ShouldQueue
             'has sent you a connection request'
         );
 
-        $receiver_id = $connection_request?->receiver_id ?? null;
+        $other_party_user = User::find($notification->additional_data['sender_id']);
         $chat_id = $connection_request?->chat_id ?? null;
-        $other_party_user = $receiver_id ? $connection_request->receiver : null;
         
-        $uid = $other_party_user?->uid ?? null;
-
         $data = [
             'type' => $notification->type,
             'title' => $notification->title,
@@ -52,8 +50,8 @@ class NotificationObserver implements ShouldQueue
             'is_read' => false,
             'notification_id' => $notification->id,
             'status' => $notification->status,
-            'other_part_id' => $receiver_id,
-            'other_part_uid' => $uid,
+            'other_party_id' => $other_party_user->id,
+            'other_party_uid' => $other_party_user->uid,
             'post_id' => $notification->additional_data['post_id'] ?? null,
             'chat_id' => $chat_id,
         ];
@@ -87,11 +85,8 @@ class NotificationObserver implements ShouldQueue
             'has sent you a connection request'
         );
 
-        $receiver_id = $connection_request?->receiver_id ?? null;
+        $other_party_user = User::find($notification->additional_data['sender_id']);
         $chat_id = $connection_request?->chat_id ?? null;
-        $other_party_user = $receiver_id ? $connection_request->receiver : null;
-
-        $uid = $other_party_user?->uid ?? null;
 
         $data = [
             'type' => $notification->type,
@@ -102,8 +97,8 @@ class NotificationObserver implements ShouldQueue
             'is_connection_request_accepted' => $is_request_accepted,
             'is_connection_request_rejected' => $is_request_rejected,
             'sender_id' => $notification->additional_data['sender_id'] ?? null,
-            'other_part_id' => $receiver_id,
-            'other_part_uid' => $uid,
+            'other_party_id' => $other_party_user->id,
+            'other_party_uid' => $other_party_user->uid,
             'status' => $notification->status,
             'post_id' => $notification->additional_data['post_id'] ?? null,
             'chat_id' => $chat_id,
