b705004f707168a491058d301faca216a56fe1b2
smith-jhonson-hub <fahad.naeem@launchbox.pk>
2025-01-22 15:28:18 +0500

    Refactor(Admin Orders): Implemented pagination

--- app/Services/Admin/Orders.php
+++ app/Services/Admin/Orders.php
@@ -18,33 +18,38 @@ class Orders
             })
         ->whereNotNull('transaction_id')
         ->orderBy('created_at', 'desc')
-        ->get();
-
-        $orders = [
-            'all_orders' => [],
-            'item_orders' => [],
-            'service_orders' => [],
-        ];
-
-        foreach($order_history as $order)
-        {
-            $order_data = [
-                'type' => $order->bid->post->type,
-                'order_by' => $order->bid->post->user->full_name,
-                'listed_by' => $order->bid->user->full_name,
-                'sale_price' => $order->bid->amount,
-                'created_at' => $order->created_at->format('Y-m-d h:i'),
-            ];
-
-            $orders['all_orders'][] = $order_data;
-
-            if ($order->bid->post->type == 'item') {
-                $orders['item_orders'][] = $order_data;
-            } else {
-                $orders['service_orders'][] = $order_data;
+        ->paginate(3);
+    
+        $all_orders = [];
+        $item_orders = [];
+        $service_orders = [];
+    
+        $order_history->getCollection()->each(
+            function ($order) use (&$all_orders, &$item_orders, &$service_orders) {
+                $order_data = [
+                    'type' => $order->bid->post->type,
+                    'order_by' => $order->bid->post->user->full_name,
+                    'listed_by' => $order->bid->user->full_name,
+                    'sale_price' => $order->bid->amount,
+                    'created_at' => $order->created_at->format('Y-m-d h:i'),
+                ];
+    
+                $all_orders[] = $order_data;
+    
+                if ($order->bid->post->type == 'item') {
+                    $item_orders[] = $order_data;
+                } elseif ($order->bid->post->type == 'service') {
+                    $service_orders[] = $order_data;
+                }
             }
-        }
-
-        return $orders;
-    }
+        );
+    
+        $order_history->setCollection(collect([
+            'all_orders' => $all_orders,
+            'item_orders' => $item_orders,
+            'service_orders' => $service_orders,
+        ]));
+    
+        return $order_history;
+    }    
 }
